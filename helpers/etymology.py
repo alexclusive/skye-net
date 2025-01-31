80dbfe99d23b6659cd1e666a9816be2765cf66ac
alexclusive <taylorpalex@gmail.com>
2025-01-31 16:25:47 +1100

    Add word into result of etymology

--- helpers/etymology.py
+++ helpers/etymology.py
@@ -3,15 +3,16 @@ import ety
 
 def get_etymology(word):
 	try:
-		state = "Before word url parse"
 		url_safe_word = urllib.parse.quote(word)
+
 		etymonline_search = "https://etymonline.com/search?q=" + url_safe_word
 		etymonline_result = "https://etymonline.com/word/" + url_safe_word
 
-		state = "Before ety tree"
 		ety_result = str(ety.tree(word))
+		if len(ety_result) == 0:
+			ety_result = "No tree found"
 
-		formatted = f"Etymonline:\nSearch: <{etymonline_search}>\nResult: {etymonline_result}\n\nEty tree:\n{ety_result}"
+		formatted = f"Etymology for '{word}'\n\nEtymonline:\nSearch: <{etymonline_search}>\nResult: {etymonline_result}\n\nEty tree:\n{ety_result}"
 		return formatted
 	except Exception as e:
 		return f"Error occured while fetching etymology for {word}.\nState: {state}.\nError: {e}"
